e10a8d83f904d91bdbe0adbc52f9a288f7c9ce2f
wawancenggoro <wawancenggoro@gmail.com>
2026-02-01 16:21:51 +0700

    update

--- docker/commands.sh
+++ docker/commands.sh
@@ -1,8 +1,9 @@
-docker pull osgeo/gdal:ubuntu-small-3.6.3
-docker images
+sudo docker pull osgeo/gdal:ubuntu-small-3.6.3
+sudo docker images
 
-docker run -dit --name gdal_container osgeo/gdal:ubuntu-small-3.6.3 bash
-docker exec -it gdal_container bash # enter the container
+sudo docker run -dit --name gdal_container osgeo/gdal:ubuntu-small-3.6.3 bash
+sudo docker ps
+sudo docker exec -it gdal_container bash # enter the container
 
 #==========================================================================
 # run this inside the container
@@ -18,14 +19,15 @@ pip install pandas
 #==========================================================================
 # press ctrl+d to exit from container and run this outside of the container
 #==========================================================================
-docker cp create_shape.py gdal_container:/workspace
-docker cp read_shape.py gdal_container:/workspace
+sudo docker cp create_shape.py gdal_container:/workspace
+sudo docker cp read_shape.py gdal_container:/workspace
 # then enter the container again to test running the python script
 #==========================================================================
 
-docker commit gdal_container wawancenggoro/gdal_pandas:latest
-docker push wawancenggoro/gdal_pandas:latest
+sudo docker login
+sudo docker commit gdal_container wawancenggoro/gdal_pandas:latest
+sudo docker push wawancenggoro/gdal_pandas:latest
 
-docker rm -f gdal_container
-docker rmi -f wawancenggoro/gdal_pandas:latest
-docker rmi -f osgeo/gdal:ubuntu-small-3.6.3
+sudo docker rm -f gdal_container
+sudo docker rmi -f wawancenggoro/gdal_pandas:latest
+sudo docker rmi -f osgeo/gdal:ubuntu-small-3.6.3
--- venv/commands.sh
+++ venv/commands.sh
@@ -1,8 +1,10 @@
 # venv
-python -m venv my_new_venv
-./my_new_venv/Scripts/activate
+sudo apt install python3.10-venv
+python3 -m venv my_new_venv
+source my_new_venv/bin/activate
 pip install pandas
 pip freeze
+deactivate
 
 # conda
 # install miniconda: https://www.anaconda.com/docs/getting-started/miniconda/install#linux-2
